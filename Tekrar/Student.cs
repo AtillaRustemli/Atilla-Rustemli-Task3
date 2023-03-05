ede3f7084dc19cac2e21fc6b8fd357716fffb1f8
Atilla <rustemliatilla662@gmail.com>
2023-03-05 18:45:02 +0400

    Add project files.

---
+++ Tekrar/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RustemliAtillaTask3
+{
+    internal class Student
+    {
+        public string name;
+        public string surname;
+        public string speciality;
+        public string groupNo;
+        public int age;
+    }
+}
