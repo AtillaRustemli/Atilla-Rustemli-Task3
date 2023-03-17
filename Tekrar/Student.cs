0abe258453c7bfb141eebb50c080d18642eab026
AtillaRustemli <125785696+AtillaRustemli@users.noreply.github.com>
2023-03-17 11:01:29 +0400

    Update Student.cs

--- Tekrar/Student.cs
+++ Tekrar/Student.cs
@@ -8,10 +8,15 @@ namespace RustemliAtillaTask3
 {
     internal class Student
     {
-        public string name;
+         public string name;
         public string surname;
         public string speciality;
         public string groupNo;
         public int age;
+
+        public override string ToString()
+        {
+            return $"{name} {surname} {speciality} {groupNo} {age} ";
+        }
     }
 }
