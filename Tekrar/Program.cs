8d0352e2a283d7d773ad67f0aa24681e065770cf
AtillaRustemli <125785696+AtillaRustemli@users.noreply.github.com>
2023-03-17 11:02:31 +0400

    Update Program.cs

--- Tekrar/Program.cs
+++ Tekrar/Program.cs
@@ -4,38 +4,38 @@
     {
         static void Main(string[] args)
         {
-            int say;
+           int say;
         l1:
             Console.Write("Telebelerin sayini qeyd edin: ");
-            if(!int.TryParse(Console.ReadLine(),out say) || say <= 0)
+            if (!int.TryParse(Console.ReadLine(), out say) || say <= 0)
             {
                 goto l1;
             }
-            Student[] information=new Student[say];
-            for(int i=0;i<information.Length;i++)
+            test[] information = new test[say];
+            for (int i = 0; i < information.Length; i++)
             {
-                Student inform = new Student();
+                test inform = new test();
 
-                Console.Write($"{i+1}.Telebenin adi: ");
+                Console.Write($"{i + 1}.Telebenin adi: ");
                 inform.name = Console.ReadLine();
                 Console.Write($"{i + 1}.Telebenin soyadi: ");
                 inform.surname = Console.ReadLine();
-                l2:
+            l2:
                 Console.Write($"{i + 1}.Telebenin yashi: ");
-                if(!int.TryParse(Console.ReadLine(),out inform.age))
+                if (!int.TryParse(Console.ReadLine(), out inform.age))
                 {
                     goto l2;
                 }
                 Console.Write($"{i + 1}.Telebenin ixtisasi: ");
                 inform.speciality = Console.ReadLine();
+
                 Console.Write($"{i + 1}.Telebenin qrup nomresi: ");
                 inform.groupNo = Console.ReadLine();
 
 
                 information[i] = inform;
-                
+                Console.WriteLine($"{information[i]} //");
             }
-            Console.WriteLine(information);
 
         }
     }
